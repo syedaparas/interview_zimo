6fbdae4e51468ec17c10df8504f9cb32fd9bec70
syedaparas <af5167735@gmail.com>
2023-03-16 13:24:32 +0500

    Zimo About

--- components/Global.js
+++ components/Global.js
@@ -1,20 +1,20 @@
 import Image from 'next/image'
 
-export default function Global(){
+export default function Global() {
     return (
-        <div> 
         <div>
-            <Image 
-            src='/images/World Map (dots).png'
-            width={1400}
-            height={400}
-            />
-        </div>
-        <div className='md:relative md:bottom-[500px]'>
-            <h1 className=' font-meduim text-3xl p-8'>GLOBAL VISION</h1>
-            <p className='text-center p-3 text-meduim font-Lato text-[#07030b]'>WITH A REMOTE CULTURE, DIVERSITY IS NATURALLY IN OUR DNA.<br/> 
-BAS ED ACROSS THE GLOBE, MAKING UP OVER 23 DIFFERENT NATIONALITIES.</p>
-        </div>
+            <div>
+                <Image
+                    src='/images/World Map (dots).png'
+                    width={1400}
+                    height={400}
+                />
+            </div>
+            <div className='md:relative md:bottom-[500px]'>
+                <h1 className=' font-meduim text-3xl p-8'>GLOBAL VISION</h1>
+                <p className='text-center p-3 text-meduim font-Lato text-[#07030b]'>WITH A REMOTE CULTURE, DIVERSITY IS NATURALLY IN OUR DNA.<br />
+                    BAS ED ACROSS THE GLOBE, MAKING UP OVER 23 DIFFERENT NATIONALITIES.</p>
+            </div>
         </div>
     )
 }
--- components/PartnerCompanies.js
+++ components/PartnerCompanies.js
@@ -1,56 +1,56 @@
 import Image from 'next/image'
 
-export default function PartnerCompanies(){
+export default function PartnerCompanies() {
     return (
         <>
-        <hr className='divide-black'/>
-        <div className='h-[100%] p-[300px] flex flex-row justify-between'>
-            <div className='flex flex-col justify-evenly'> 
-            <Image 
-            src='/logos/ZIMO B.png'
-            width={160}
-            height={160}
-            />
-            <p className='text-[#696969] font-meduim text-3xl p-5 px-0'>PARTNERS</p>
+            <hr className='divide-black' />
+            <div className='h-[100%] p-[300px] flex flex-row justify-between'>
+                <div className='flex flex-col justify-evenly'>
+                    <Image
+                        src='/logos/ZIMO B.png'
+                        width={160}
+                        height={160}
+                    />
+                    <p className='text-[#696969] font-meduim text-3xl p-5 px-0'>PARTNERS</p>
+                </div>
+                <div className='flex flex-col justify-evenly'>
+                    <Image
+                        src='/logos/CIVICA CES B.png'
+                        width={180}
+                        height={180}
+                    />
+                    <Image className='px-5'
+                        src='/logos/Barclays.png'
+                        width={180}
+                        height={180}
+                    />
+                </div>
             </div>
-            <div  className='flex flex-col justify-evenly'> 
-            <Image
-            src='/logos/CIVICA CES B.png'
-            width={180}
-            height={180}
-            />
-            <Image className='px-5'
-            src='/logos/Barclays.png'
-            width={180}
-            height={180}
-            />
+            <div className='h-full bg-[#eeecec]'>
+                <div className='bg-contain'>
+                    <Image
+                        src='/images/Yacht 1822.jpg'
+                        width={1400}
+                        height={100}
+                    />
+                </div>
+                <div className='flex flex-row relative bottom-[300px]'>
+                    <div className='w-full h-[50vh] p-[5%] py-0'>
+                        <Image className='rounded-2xl'
+                            src='/images/Car 1821.jpg'
+                            width={400}
+                            height={100}
+                        />
+                    </div>
+                    <div className='absolute right-[5%] top-[70%] opacity-50 text-[#E6E6E6]'>
+                        <Image
+                            src='/logos/ZIMO ZIG W.png'
+                            width={550}
+                            height={700}
+                        />
+                    </div>
+                </div>
             </div>
-        </div>
-        <div className='h-full bg-[#eeecec]'>
-            <div className='bg-contain'>
-                <Image
-                src='/images/Yacht 1822.jpg'
-                width={1400}
-                height={100}
-                />
-            </div>
-            <div className='flex flex-row relative bottom-[300px]'> 
-            <div className='w-full h-[50vh] p-[5%] py-0'>
-                <Image className='rounded-2xl'
-                src='/images/Car 1821.jpg'
-                width={400}
-                height={100}
-                />
-            </div>
-            <div className='absolute right-[5%] top-[70%] opacity-50 text-[#E6E6E6]'>
-                <Image
-                src='/logos/ZIMO ZIG W.png'
-                width={550}
-                height={700}
-                />
-            </div>
-            </div>
-        </div>
         </>
     )
 }
